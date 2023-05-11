a3ce209581275121dce5b6c79bf3c17870b3cb4b
okancc <118931286+okancc@users.noreply.github.com>
2023-05-11 16:27:20 +0300

    gerekli güncellemeler yapıldı.Partial View eklendi.

---
+++ TraversalProje/Views/ViewComponents/Default/_Feature.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.Concrete;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProje.Views.ViewComponents.Default
+{
+    public class _Feature:ViewComponent
+    {
+        FeatureManager featureManager = new FeatureManager(new EfFeatureDal()); 
+        public IViewComponentResult Invoke()
+        {
+           
+
+            return View();
+
+        }
+    }
+}
---
+++ TraversalProje/Views/ViewComponents/Default/_Testimonial.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TraversalProje.Views.ViewComponents.Default
+{
+    public class _Testimonial:ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+
+        }
+
+    }
+}
